728532077f079b8c66617b9a3a1b4b8cca1e6387
Philippe CHARRIERE <ph.charriere@gmail.com>
2014-02-10 14:43:14 +0100

    Update hi.bat
    fix

--- hi.bat
+++ hi.bat
@@ -2,4 +2,4 @@
 @REM Copyright 2014-2015 @k33g_org
 @REM echo %CD% 	-> current directory
 @REM echo %~dp0	-> base directory
-java -jar %~dp0/hi.jar %~dp0 %CD% %0%
+java -jar %~dp0/hi.jar %~dp0 %CD% %1
